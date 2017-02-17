a3d1d01ee7194b22d8037190dc6ff285f1fef45d
Luis Antonio Ramirez Martinez <luis@blacklabs.mx>
2017-02-16 20:47:51 -0700

    Ïmplementación de un motor de templates.

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var express = require('express');
 
 var app = express();
-
+app.set("view engine", "pug");
 
 app.get('/', function(req, res) {
-  res.send("Hola Mundo!");
+  var nombre = new Object();
+  nombre.primerNombre = "Luis ";
+  nombre.apellido = "Ramirez";
+
+  var numero1 = 4;
+  var numero2 = 8;
+
+  var paises = ["México", "China", "Canada", "Papua Nueva Guinea"];
+
+
+  res.render("index", {nombre:nombre, numero1:numero1,
+     numero2:numero2, paises:paises});
 });
 
 app.listen(8080);
